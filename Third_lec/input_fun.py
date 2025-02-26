321aeb7a873eadf3d30c6298fafcaa4b607ab771
Bilal0335 <bilalhussain42201.com>
2025-02-26 17:20:11 +0500

    update

---
+++ Third_lec/input_fun.py
@@ -0,0 +1,15 @@
+# Input function in Python | Take User Input in Python 
+
+'''
+input:
+syntx = input(propmt(optional))
+e.g: input("Enter your name")
+'''
+
+
+# input("enter your name")
+
+# print("Hello", input("Enter your name? "))
+
+
+print("Hey"+" "+input("Enter your name ")+" "+"How are you?")

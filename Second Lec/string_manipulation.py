3d09ec04ade7696da31c6e21c5457a80f0bae98d
Bilal0335 <bilalhussain42201.com>
2025-02-26 17:11:30 +0500

    update

---
+++ Second Lec/string_manipulation.py
@@ -0,0 +1,15 @@
+# print() function & String concatenation(using +) in Python
+'''
+\n --> new line
+print("hello"+"world") --> helloword --> no spaces
+print("hello"+" "+"world") --> hello world
+
+'''
+
+print("hello \n world")
+
+print("hello"+"world")
+
+print("hello"+" " +"world")
+
+    #   print("Hello world") #IndentationError: unexpected indent

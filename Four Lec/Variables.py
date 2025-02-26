ecf024ce708c923efaa62a45a320409e525f5733
Bilal0335 <bilalhussain42201.com>
2025-02-26 17:34:36 +0500

    update

---
+++ Four Lec/Variables.py
@@ -0,0 +1,23 @@
+# Variables in Python
+'''
+ a variable is a name that stores a value. It acts as a container for data.
+ 
+syntax:
+variable_name = value
+
+'''
+
+name = input("Enter Your Name? ")
+age = input("Enter Your Age? ")
+print("Hello" +" " + name + "!" + " \n" + "My age" + " "+ age + "!")
+
+length = len(name)
+print(length)
+
+
+
+# a = 1
+
+# b = "flower"
+
+# print(a + b) # typeError int  and str

ebe05b24600679b43cf1e006589998fcbd9e6a1f
Bilal0335 <bilalhussain42201.com>
2025-02-27 13:11:31 +0500

    update

--- Exercise #3/ex_03.py
+++ Exercise #3/ex_03.py
@@ -7,4 +7,9 @@ digit_2 = int(number[1])
 
 print("Sum of digits:", digit_1 + digit_2)
 
+# Second Method to add digits of a number
+number = int(input("Enter a two-digit number: "))  
+digit_1 = number // 10  
+digit_2 = number % 10   
 
+print("Sum of digits:", digit_1 + digit_2)

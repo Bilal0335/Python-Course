928a92ce1643053de84621a7623ef06017f9a1f3
Bilal0335 <bilalhussain42201.com>
2025-02-27 12:42:38 +0500

    update

---
+++ Six Lec/Data_types.py
@@ -0,0 +1,39 @@
+# Data Types in python
+
+# var_1 = 36366366363668282792010837873494048
+# print(var_1)
+
+# var_1 = 0o123
+# var_2 = 0x123
+# var_3 = 123
+# print(var_1)
+# print(var_2)
+# print(var_3)
+
+
+# # print("Addition:", var_1 + var_2)
+# print(type(var_1))
+# print(type(var_2))
+# print(type(var_3))
+
+
+# name = "Bilal Hussain"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# field = " CS"
+# print(name + field)
+
+# num_1 = 5
+# print(name + num_1)
+
+# print("Bilal\'s \n \"Developer\"")
+# print(5 * "\n Hello Bilal\'s")
+
+bol_1 = True
+
+print(bol_1)
+
+print(type(bol_1))

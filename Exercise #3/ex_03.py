94cb3b68aaffa79b54d3b064f04c37b1647d86ec
Bilal0335 <bilalhussain42201.com>
2025-02-27 13:08:36 +0500

    update

---
+++ Exercise #3/ex_03.py
@@ -0,0 +1,10 @@
+# Program to add digits of a number
+
+
+number = input("Enter a two-digit number: ")  
+digit_1 = int(number[0]) 
+digit_2 = int(number[1]) 
+
+print("Sum of digits:", digit_1 + digit_2)
+
+

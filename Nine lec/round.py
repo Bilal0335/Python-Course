d657de3df4b4d3fd1e49162e5f857b6aa5315b8d
Bilal0335 <bilalhussain42201.com>
2025-03-02 12:54:33 +0500

    update

---
+++ Nine lec/round.py
@@ -0,0 +1,13 @@
+# round() function in Python
+# round(number,no-of-digit)
+
+
+print(round(7))
+print(round(7,2))
+print(round(7.61))
+print(round(2.66666,2))
+print(round(2.6656,0))
+print(round(11.5))
+print(round(12.5))
+print(round(674,-1))
+print(round(674,-2))
---
+++ Ten lec/f-string.py
@@ -0,0 +1,15 @@
+# f-Strings in Python
+
+name = "Bilal"
+
+print(f"My name is {name}")
+
+# Multiple variables
+
+age = 25
+
+print(f"My name is {name} and I am {age} years old")
+
+# String formatting
+
+print(f"My name is {name.capitalize()}, and I am {age} years old")

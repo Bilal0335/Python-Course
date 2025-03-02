ebf04d4728cc789c43b16118aef157c24bc234ca
Bilal0335 <bilalhussain42201.com>
2025-03-02 21:30:49 +0500

    update

--- Ten lec/f-string.py
+++ Ten lec/f-string.py
@@ -13,3 +13,11 @@ print(f"My name is {name} and I am {age} years old")
 # String formatting
 
 print(f"My name is {name.capitalize()}, and I am {age} years old")
+
+from IPython.display import display
+
+name = "Bilal Hussain"
+display(name)
+
+name = "          Bilal Hussain         "
+display(name)
